e39cac2ef67b5386feb5276b36bc6e16189cbb7a
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-03-29 17:38:23 +0700

    Add demo api CRUD Category

---
+++ ViewModels/CategoryFormVm.cs
@@ -0,0 +1,9 @@
+﻿namespace ViewModels
+{
+    public class CategoryFormVm
+    {
+        public string NameCategory { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ ViewModels/CategoryVm.cs
@@ -0,0 +1,11 @@
+﻿namespace ViewModels
+{
+    public class CategoryVm
+    {
+        public int CategoryId { get; set; }
+
+        public string NameCategory { get; set; }
+
+        public string Description { get; set; }
+    }
+}
