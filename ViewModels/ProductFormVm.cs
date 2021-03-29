00f5882a9b80b2aa9fd08b466b38fe87f361512d
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-03-29 16:37:42 +0700

    Add ProductFormVm

---
+++ ViewModels/ProductFormVm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ViewModels
+{
+    public class ProductFormVm
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Images { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
