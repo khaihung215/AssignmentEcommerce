873469e8158afc49c4d4fe83a12c64fac0c241fa
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-03-29 17:25:06 +0700

    Add demo api CRUD Product

--- AssignmentEcommerce_API/Controllers/ProductsController.cs
+++ AssignmentEcommerce_API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using AssignmentEcommerce_API.Data;
+using AssignmentEcommerce_API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -34,6 +35,62 @@ namespace AssignmentEcommerce_API.Controllers
             }).ToListAsync();
 
             return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(ProductFormVm model)
+        {
+            var product = new Product
+            {
+                Name = model.Name,
+                Description = model.Description,
+                Price = model.Price,
+                Images = model.Images,
+                CreatedDate = DateTime.Now.Date,
+                UpdatedDate = DateTime.Now.Date,
+                CategoryId = model.CategoryId
+            };
+
+            _shopDbContext.Products.Add(product);
+            await _shopDbContext.SaveChangesAsync();
+
+            return Accepted();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, ProductFormVm model)
+        {
+            var product = await _shopDbContext.Products.FirstOrDefaultAsync(x => x.ProductId == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            product.Name = model.Name;
+            product.Price = model.Price;
+            product.Description = model.Description;
+            product.Images = model.Images;
+            product.UpdatedDate = DateTime.Now.Date;
+
+            await _shopDbContext.SaveChangesAsync();
+            return Accepted();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var product = await _shopDbContext.Products.FirstOrDefaultAsync(x => x.ProductId == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _shopDbContext.Products.Remove(product);
+
+            await _shopDbContext.SaveChangesAsync();
+            return Accepted();
         }
     }
 }
