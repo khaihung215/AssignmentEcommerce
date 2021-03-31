3b5fc498a7ecc941cf24ebfa37611120a2f4ffbc
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-03-31 14:03:19 +0700

    Add project backend

---
+++ AssignmentEcommerce_Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AssignmentEcommerce_Backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
