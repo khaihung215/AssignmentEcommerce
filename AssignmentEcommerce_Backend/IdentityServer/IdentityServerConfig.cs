b2c1c0e7f07bc16da09daa6b404750a11184969a
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-04-01 11:11:16 +0700

    IdentityServerConfig - Update client setting

--- AssignmentEcommerce_Backend/IdentityServer/IdentityServerConfig.cs
+++ AssignmentEcommerce_Backend/IdentityServer/IdentityServerConfig.cs
@@ -41,9 +41,9 @@ namespace AssignmentEcommerce_Backend.IdentityServer
 
                     AllowedGrantTypes = GrantTypes.Code,
 
-                    RedirectUris = { "https://localhost:5002/signin-oidc" },
+                    RedirectUris = { "https://localhost:44307/signin-oidc" },
 
-                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:44307/signout-callback-oidc" },
 
                     AllowedScopes = new List<string>
                     {
