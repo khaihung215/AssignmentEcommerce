9c917f5470317eff76a952bed4e5a63a4c0916c0
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-04-01 09:53:48 +0700

    Set require confirm account by email to false

--- AssignmentEcommerce_Backend/Startup.cs
+++ AssignmentEcommerce_Backend/Startup.cs
@@ -32,7 +32,7 @@ namespace AssignmentEcommerce_Backend
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
