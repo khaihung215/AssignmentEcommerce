0e01231cb00107ba298547ad9f88f93681053314
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-04-01 10:47:17 +0700

    Add Identity Server

---
+++ AssignmentEcommerce_Backend/IdentityServer/IdentityServerConfig.cs
@@ -0,0 +1,57 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace AssignmentEcommerce_Backend.IdentityServer
+{
+    public class IdentityServerConfig
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+             new ApiScope[]
+             {
+                  new ApiScope("assignmentecommerce.api", "Assignment Ecommerce API")
+             };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                // machine to machine client
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    // scopes that client has access to
+                    AllowedScopes = { "assignmentecommerce.api" }
+                },
+
+                // interactive ASP.NET Core MVC client
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "assignmentecommerce.api"
+                    }
+                }
+            };
+    }
+}
--- AssignmentEcommerce_Backend/Startup.cs
+++ AssignmentEcommerce_Backend/Startup.cs
@@ -1,4 +1,5 @@
 using AssignmentEcommerce_Backend.Data;
+using AssignmentEcommerce_Backend.IdentityServer;
 using AssignmentEcommerce_Backend.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -36,6 +37,19 @@ namespace AssignmentEcommerce_Backend
             services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
+            services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+                options.EmitStaticAudienceClaim = true;
+            }).AddInMemoryIdentityResources(IdentityServerConfig.IdentityResources)
+               .AddInMemoryApiScopes(IdentityServerConfig.ApiScopes)
+               .AddInMemoryClients(IdentityServerConfig.Clients)
+               .AddAspNetIdentity<User>()
+               .AddDeveloperSigningCredential(); // not recommended for production - you need to store your key material somewhere secure
+
             services.AddControllersWithViews();
         }
 
@@ -58,7 +72,7 @@ namespace AssignmentEcommerce_Backend
 
             app.UseRouting();
 
-            app.UseAuthentication();
+            app.UseIdentityServer();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
