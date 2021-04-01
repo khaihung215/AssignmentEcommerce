99b9e63b43d8acc6ec1230594e2a7991ea384c72
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-04-01 10:52:55 +0700

    Add project CustomerSite

---
+++ AssignmentEcommerce_CustomerSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AssignmentEcommerce_CustomerSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
