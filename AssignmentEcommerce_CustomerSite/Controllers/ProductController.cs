c4b498782bb9faab8f5ede35162c92ebb19c0eff
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-04-07 17:14:30 +0700

    UI - Add product page

---
+++ AssignmentEcommerce_CustomerSite/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using AssignmentEcommerce_CustomerSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AssignmentEcommerce_CustomerSite.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductApiClient _productClient;
+        private readonly ICategoryApiClient _categoryClient;
+
+        public ProductController(IProductApiClient productClient, ICategoryApiClient categoryClient)
+        {
+            _productClient = productClient;
+            _categoryClient = categoryClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productClient.GetProduct();
+            ViewBag.Products = products;
+
+            var categorys = await _categoryClient.GetCategory();
+            ViewBag.Categorys = categorys;
+
+            return View();
+        }
+    }
+}
