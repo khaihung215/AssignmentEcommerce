a5f7338ed5c40150d0bfca5e04234b56b59a9747
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-04-09 11:36:19 +0700

    Add cart and cartdetail model

---
+++ AssignmentEcommerce_Backend/Models/Cart.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignmentEcommerce_Backend.Models
+{
+    public class Cart
+    {
+        public string CartId { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual IdentityUser User { get; set; }
+
+        public virtual ICollection<CartDetail> CartDetails { get; set; }
+    }
+}
---
+++ AssignmentEcommerce_Backend/Models/CartDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignmentEcommerce_Backend.Models
+{
+    public class CartDetail
+    {
+        public string CartDetailId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string CartId { get; set; }
+
+        public virtual Cart Cart { get; set; }
+
+        public string ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+    }
+}
