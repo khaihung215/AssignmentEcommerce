d3b0c9fd2f85f6306765a5b5cc6eb9569e2ed589
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-04-13 11:19:17 +0700

    Add class SqliteInMemoryFixture

---
+++ AssignmentEcommerce_Test/SqliteInMemoryFixture.cs
@@ -0,0 +1,56 @@
+﻿using AssignmentEcommerce_Backend.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AssignmentEcommerce_Test
+{
+    class SqliteInMemoryFixture: IDisposable
+    {
+        private IServiceScope _serviceScope;
+        private SqliteConnection _connection;
+
+        public virtual IServiceProvider ServiceProvider
+        {
+            get
+            {
+                if (_serviceScope == null)
+                {
+                    _serviceScope = ConfigureServices(new ServiceCollection())
+                        .BuildServiceProvider()
+                        .CreateScope();
+                }
+
+                return _serviceScope.ServiceProvider;
+            }
+        }
+
+        public virtual ApplicationDbContext Context
+            => ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        public virtual void CreateDatabase()
+        {
+            Dispose();
+            _connection = new SqliteConnection("Data Source=:memory:");
+            _connection.Open();
+            Context.Database.EnsureCreated();
+        }
+
+        public virtual void Dispose()
+        {
+            _connection?.Close();
+            _connection?.Dispose();
+            _serviceScope?.Dispose();
+            _serviceScope = null;
+        }
+
+        public virtual IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(b =>
+                b.UseSqlite(_connection));
+
+            return services;
+        }
+    }
+}
