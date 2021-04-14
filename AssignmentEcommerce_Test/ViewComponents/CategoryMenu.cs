bd421ea1b01498e7fa69ccf930924067c3c36f56
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-04-14 21:59:57 +0700

    Add test CategoryMenuViewComponent

---
+++ AssignmentEcommerce_Test/ViewComponents/CategoryMenu.cs
@@ -0,0 +1,52 @@
+﻿using AssignmentEcommerce_CustomerSite.Services;
+using AssignmentEcommerce_CustomerSite.ViewComponents;
+using AssignmentEcommerce_Shared;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AssignmentEcommerce_Test.ViewComponents
+{
+    public class CategoryMenu : IClassFixture<SqliteInMemoryFixture>
+    {
+        private readonly SqliteInMemoryFixture _fixture;
+
+        public CategoryMenu(SqliteInMemoryFixture fixture)
+        {
+            _fixture = fixture;
+            _fixture.CreateDatabase();
+        }
+
+        [Fact]
+        public async Task CategoryMenu_ShouldReturns_NotNullView()
+        {
+            //Arrange
+            var _categoryApiClient = Mock.Of<ICategoryApiClient>();
+
+            var viewComponent = new CategoryMenuViewComponent(_categoryApiClient);
+
+            //Act
+            var result = await viewComponent.InvokeAsync() as ViewViewComponentResult;
+
+            //Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task CategoryMenu_ShouldReturns_CorrectViewType()
+        {
+            //Arrange
+            var _categoryApiClient = Mock.Of<ICategoryApiClient>();
+
+            var viewComponent = new CategoryMenuViewComponent(_categoryApiClient);
+
+            //Act
+            var result = await viewComponent.InvokeAsync() as ViewViewComponentResult;
+
+            //Assert
+            Assert.IsType<ViewViewComponentResult>(result);
+        }
+    }
+}
