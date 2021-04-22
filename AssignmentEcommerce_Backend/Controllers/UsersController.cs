aae97f3c759e283ab1249cc6ccf3d517689f7b82
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-04-22 20:45:53 +0700

    Add user api

---
+++ AssignmentEcommerce_Backend/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using AssignmentEcommerce_Backend.Data;
+using AssignmentEcommerce_Backend.Models;
+using AssignmentEcommerce_Backend.Services;
+using AssignmentEcommerce_Shared;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AssignmentEcommerce_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize("Bearer")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IEnumerable<IdentityUser>> GetUsers()
+        {
+            var users = await _context.Users.AsNoTracking().ToListAsync();
+
+            return users;
+        }
+    }
+}
