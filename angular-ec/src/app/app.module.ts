64bb76761672301982b4b3f8e63f3fe7f00ab699
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-05-27 21:20:18 +0700

    Add component footer

--- angular-ec/src/app/app.module.ts
+++ angular-ec/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
