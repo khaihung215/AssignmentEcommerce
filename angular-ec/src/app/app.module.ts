8e55dd655f89b05971d36778ea928a5c137d12c0
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-05-27 22:52:19 +0700

    Add component carousel

--- angular-ec/src/app/app.module.ts
+++ angular-ec/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { CarouselComponent } from './home/carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
