1c2f2ae6fe0bade8cb7fad472a5a6507d2369c8b
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-05-27 22:51:21 +0700

    Add component home

--- angular-ec/src/app/app.module.ts
+++ angular-ec/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
