cb8e112f12ff974b219a4fab5c2568cc912568a2
LAPTOP-7UR8958C\Admin <hungluong369@gmail.com>
2021-05-27 23:12:10 +0700

    Custom component carousel

--- angular-ec/src/app/home/carousel/carousel.component.ts
+++ angular-ec/src/app/home/carousel/carousel.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent implements OnInit {
+  slide1 = 'assets/images/slideshow_1.jpg'
+  slide2 = 'assets/images/slideshow_2.jpg'
+  slide3 = 'assets/images/slideshow_3.jpg'
+
 
   constructor() { }
 
