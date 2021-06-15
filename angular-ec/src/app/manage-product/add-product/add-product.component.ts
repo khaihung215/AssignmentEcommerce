7054c809921f18032e3c9b1d6b2746c6889ad88b
Khai Hung <hungluong369@gmail.com>
2021-06-15 16:28:37 +0700

    Integrate API into FE add product

--- angular-ec/src/app/manage-product/add-product/add-product.component.ts
+++ angular-ec/src/app/manage-product/add-product/add-product.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Category } from 'src/models/category';
+import { CategoryService } from 'src/services/categoryService';
+import { ProductService } from 'src/services/productService';
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.css']
+  styleUrls: ['./add-product.component.css'],
+  providers: [CategoryService, ProductService],
 })
 export class AddProductComponent implements OnInit {
+  addProductForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    price: new FormControl('', [Validators.required]),
+    categoryId: new FormControl('', [Validators.required]),
+    images: new FormControl(''),
+  });
 
-  constructor() { }
+  listCategories: Category[];
 
-  ngOnInit(): void {
+  constructor(
+    private categoryService: CategoryService,
+    private productService: ProductService
+  ) {}
+
+  get f() {
+    return this.addProductForm.controls;
   }
 
+  ngOnInit(): void {
+    this.categoryService
+      .getCategories()
+      .subscribe((categories) => (this.listCategories = categories));
+  }
+
+  onSubmit() {
+    var formData = new FormData();
+
+    Object.keys(this.addProductForm.value).forEach((key) => {
+      formData.append(key, this.addProductForm.value[key]);
+    });
+
+    this.productService.addProduct(formData);
+  }
+
+  onFileChange(event) {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0] as File;
+      this.addProductForm.get('images').setValue(file);
+    }
+  }
 }
--- angular-ec/src/services/productService.ts
+++ angular-ec/src/services/productService.ts
@@ -4,12 +4,13 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from 'src/models/product';
 import { map } from 'rxjs/operators';
+import { ToastService } from 'angular-toastify';
 
 const product_url = backEnd_Url + '/api/product';
 
 @Injectable()
 export class ProductService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private toastService: ToastService) {}
 
   getProducts(): Observable<Product[]> {
     return this.http.get(product_url).pipe(map((response: any) => response));
@@ -43,5 +44,12 @@ export class ProductService {
     return this.http
       .get(product_url + '/getnewproduct')
       .pipe(map((response: any) => response));
+  }
+
+  addProduct(formData: any) {
+    return this.http.post(product_url, formData).subscribe(
+      (response) => this.toastService.success('Thêm sản phẩm thành công !'),
+      (error) => this.toastService.error('Thêm sản phẩm thất bại !')
+    );
   }
 }
